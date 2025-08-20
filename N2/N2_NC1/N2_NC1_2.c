c8a78e00f60456a87fe70ef22fed88c0391e3641
Gutierre Quaresma <108359160+GutQuaresma@users.noreply.github.com>
2025-08-20 14:14:41 +0000

    estudo dia 20/08

--- N2/N2_NC1/N2_NC1_2.c
+++ N2/N2_NC1/N2_NC1_2.c
@@ -8,5 +8,25 @@ int main(){
     Atribuição com Multiplicação (*=)
     Atribuição com Divisão (/=)
     */
+   int numero1 = 10, numero2, resultado;
+
+   resultado = 10;
+   printf("Resultado: %d\n", resultado);
+
+   //resultado = resultado + 20;
+   resultado += 20;
+   printf("Resultado: %d\n", resultado);
+
+   //resultado = resultado - numero1;
+   resultado -= numero1;
+   printf("Resultado: %d\n", resultado);
+
+   //resultado = resultado * 5 (vezes);
+   resultado *= 5;
+   printf("Resultado: %d\n", resultado);
+
+   //resultado = resultado / 2 (dividido);
+   resultado /= 2;
+   printf("Resultado: %d\n", resultado);
 
 }
